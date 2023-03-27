4fd48513b64728ea86f2c622438aaf9e34ca8360
Tyler Marques <me@tylermarques.com>
2023-03-27 14:10:32 -0400

    Updated
    Signed-off-by: Tyler Marques <me@tylermarques.com>

--- scripts/obs_plus_calendar.sh
+++ scripts/obs_plus_calendar.sh
@@ -1,1 +1,1 @@
-i3 'workspace Chrome, move workspace to output HDMI-1; exec google-chrome calendar.google.com;' && sleep 1 && i3 'workspace OBS, move workspace to output HDMI-0; exec obs'
+i3 'workspace Calendar, move workspace to output HDMI-1; exec google-chrome calendar.google.com;' && sleep 1 && i3 'workspace OBS, move workspace to output HDMI-0; exec obs'
