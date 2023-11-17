db0a634c87b55ba5403ff0ba423057e7ffc0a36d
Tyler Marques <me@tylermarques.com>
2023-11-17 11:41:45 -0500

    Changing away from oh-my-tmux as some configuration option was causing tmux to hang on open with alarcritty, and quit unexpectedly otherwise
    Signed-off-by: Tyler Marques <me@tylermarques.com>

--- scripts/select-audio-output.sh
+++ scripts/select-audio-output.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-speakers=$(pactl list sinks | grep Name | awk '/extra3/ {print $2}')
-headphones=$(pactl list sinks | grep Name | awk '/stereo-game/ {print $2}')
+speakers=$(pactl list sinks | grep Name | awk '/iec958/ {print $2}')
+headphones=$(pactl list sinks | grep Name | awk '/hdmi/ {print $2}')
 
 if [[ $1 == "speakers" ]]; then
+  echo $speakers
   pactl set-default-sink "$speakers"
 fi
 
 if [[ $1 == "headphones" ]]; then
+  echo $headphones
   pactl set-default-sink "$headphones"
 fi
 
