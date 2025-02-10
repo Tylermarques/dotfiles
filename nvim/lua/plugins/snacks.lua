a0c62c0136aa01c73836778c42e8245b268ceded
Tyler Marques <me@tylermarques.com>
2025-02-10 13:06:31 -0500

    Updating btscript, and removing scroll animation from nvim

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,6 +1,6 @@
 return {
   "folke/snacks.nvim",
   opts = {
-    animate = false,
+    scroll = { enabled = false },
   },
 }
--- scripts/toggle-airpods-bt-con.sh
+++ scripts/toggle-airpods-bt-con.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-DEVICE_MAC="00:F3:9F:72:EC:4C"  # Replace with your device's MAC address
+DEVICE_MAC="00:F3:9F:72:EC:4C" # Replace with your device's MAC address
 
 # Check if the device is connected
 IS_CONNECTED=$(echo -e "info $DEVICE_MAC\nexit" | bluetoothctl | grep "Connected:" | awk '{print $2}')
 
 if [ "$IS_CONNECTED" == "yes" ]; then
-    echo -e "disconnect $DEVICE_MAC\nexit" | bluetoothctl
+  bluetoothctl disconnect $DEVICE_MAC
 else
-    echo -e "connect $DEVICE_MAC\nexit" | bluetoothctl
+  bluetoothctl connect $DEVICE_MAC
 fi
